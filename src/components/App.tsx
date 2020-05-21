2d93ac3e18fbfbb5d36b81e700fd78104dc19733
akuto aoyama <aaaa.akuto@gmail.com>
2020-05-21 14:39:01 +0900

    コンポーネント分割

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,76 +1,9 @@
-// import React from 'react'
-
-// export default function App() {
-//   return (
-//     <div className="reactApp">
-//         HELLO React
-//     </div>
-//   )
-// }
 import * as React from "react";
-import  { useState } from 'react';
-import * as ReactDOM from 'react-dom';
+import { HomePage } from './HomePage';
+
 // import './App.scss'
-import { Button } from '@zeit-ui/react'
-// import { Image } from '@zeit-ui/react'
-import { useToasts } from '@zeit-ui/react'
-import { ZeitProvider, CssBaseline } from '@zeit-ui/react'
+  import { ZeitProvider, CssBaseline } from '@zeit-ui/react'
 
-interface Props {
-  text: string,
-}
-
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-function ZeitBtn() {
-  return <Button>Click MeClick MeClick MeClick Me</Button>;
-}
-
-// function SiteImage() {
-//   return <Image width="540" height="160"
-//     src="http://www.deelay.me/2000/https://react.zeit-ui.co/images/custom-domains.png" />;
-// }
-
-function Text(props: Props) {
-  return <div style={{ color: 'green' }}>{props.text}</div>;
-}
-
-
-function ZeitToast() {
-  const [toasts, setToast] = useToasts()
-  const click = () => setToast({ text: 'The Evil Rabbit jumped over the fence.' })
-  return <Button onClick={click}>Show Toast</Button>
-}
-
-
-
-
-
-
-function HomePage() {
-  const [text, setText] = useState('');
-
-  return (
-    <div>
-      <h1>
-        <Text text={text} />
-        入力して下さい
-      </h1>
-      <ZeitBtn />
-      {/* <SiteImage /> */}
-      <ZeitToast />
-      <input
-        type="text"
-        value={text}
-        onChange={e => {
-          setText(e.target.value);
-        }}
-      />
-    </div>
-  );
-}
-// ReactDOM.render(<HomePage />, document.getElementById('root'));
 export function App() {
   return(
     <ZeitProvider>
@@ -80,13 +13,3 @@ export function App() {
   )
 }
 
-
-// export const Index = () => {
-//   return <div>name</div>;
-// };
-// ReactDOM.render(<Index />, document.getElementById('index'));
-
-// import { Button } from '@zeit-ui/react'
-
-// export const MyComponent = () => <Button>Click Me</Button>
-// ReactDOM.render(<MyComponent />, document.getElementById('button'));
